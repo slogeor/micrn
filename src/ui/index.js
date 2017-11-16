d4721e32fa040b6e16b634d1259f0de2b0231de9
xiao.geng <xiao.geng@wormpex.com>
2017-11-16 09:15:49 +0800

    update

--- src/ui/index.js
+++ src/ui/index.js
@@ -10,31 +10,31 @@ export { default as Btn } from './Btn';
 export { default as Tab } from './Tab';
 // 遮罩层组件
 export { default as Mask } from './Mask';
-// Radio 单选框
+// Radio 单选框组件
 export { default as Radio } from './Radio';
-// Alert 组件
+// Alert 对话框组件
 export { default as Alert } from './Alert';
 // 数字标记组件
 export { default as Badge } from './Badge';
-// 文本框
+// 输入框组件
 export { default as Input } from './Input';
 // 导航栏组件
 export { default as NavBar } from './NavBar';
-// 对话框
+// 对话框组件
 export { default as Dialog } from './Dialog';
-// 开关
+// 开关组件
 export { default as Switch } from './Switch';
-// Checkbox 多选框
+// Checkbox 多选框组件
 export { default as Checkbox } from './Checkbox';
 // Tab item 组件
 export { default as TabItem } from './TabItem';
-// 加载ing 组件
+// 加载菊花组件
 export { default as Loading } from './Loading';
 // 提示组件
 export { default as ToolTip } from './ToolTip';
 // 底部弹出组件
 export { default as Popover } from './Popover';
-// confirm 确认框
+// confirm 确认框组件
 export { default as Confirm } from './Confirm';
 // 上拉菜单组件
 export { default as ActionSheet } from './ActionSheet';
